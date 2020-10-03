e87ee8521d374f591ec3e5d89ea105d481b8bf37
gabbo <gabbo@gabrielapilegi.local>
2020-10-03 19:39:48 -0300

    add swagger

--- src/comment/handler.clj
+++ src/comment/handler.clj
@@ -1,17 +1,43 @@
 (ns comment.handler
-  (:require [reitit.core :as r]
-            [reitit.ring :as ring]))
+  (:require [muuntaja.core :as m]
+            [reitit.core :as r]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
+            [ring.adapter.jetty :as jetty]))
 
 (def routes
-  [["/ping" {:get (fn [request] {:status 200 :body "ok"})}]])
+  [["/api"
+    {:swagger {:tags ["API"]}}
+    ["/ping"
+     {:get {:handler (fn [request] {:status 200 :body "Pong"})
+            :swagger {:title "Ping"
+                      :description "Ping"}}}]]
+   ["/swagger.json"
+    {:get {:handler (swagger/create-swagger-handler)
+           :no-doc  true
+           :swagger {:title "Comment system API"
+                     :description "Comment system API"}}}]])
+
 
 (def router
-  (ring/router routes))
+  (ring/router routes
+               {:data {:muuntaja m/instance
+                       :middleware [muuntaja/format-middleware]}}))
 
 (def app
-  (ring/ring-handler router))
+  (ring/ring-handler router
+                     (ring/routes (swagger-ui/create-swagger-ui-handler
+                                   {:path "/"}))))
+
+
+(defn start []
+  (jetty/run-jetty #'app {:port 3000 :join? false})
+  (println "Server running on port 3000"))
 
 (comment
+  (start)
   (app {:request-method :get :uri "/pring"})
   (r/match-by-path (r/router routes) "/api/")
   (r/match-by-path router "/api/")
