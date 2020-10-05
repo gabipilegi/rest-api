89d0d7ca6ebba076dff5efd177b8f33cb433f825
gabbo <gabbo@gabrielapilegi.local>
2020-10-04 21:31:06 -0300

    Add coercion

--- src/comment/handler.clj
+++ src/comment/handler.clj
@@ -1,7 +1,9 @@
 (ns comment.handler
   (:require [muuntaja.core :as m]
-            [reitit.core :as r]
+            [reitit.coercion.spec :as coercion.spec]
             [reitit.ring :as ring]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring.middleware.exception :as exception]
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.swagger :as swagger]
             [reitit.swagger-ui :as swagger-ui]
@@ -19,22 +21,40 @@
     [""
      {:get  {:summary "Get all comments"
              :handler ok}
-      :post {:summary "Create a new comment"
-             :handler ok}}]
-    ["/slug"
-     {:get {:summary "Get comments by slug"
-            :handler ok}}]
+      :post {:summary    "Create a new comment"
+             :handler    ok
+             :parameters {:body {:name              string?
+                                 :slug              string?
+                                 :text              string?
+                                 :parent-comment-id int?}}
+             :responses  {200 {:body string?}}}}]
+    ["/slug/:slug"
+     {:get {:summary    "Get comments by slug"
+            :handler    ok
+            :parameters {:path {:slug string?}}}}]
     ["/id/:id"
-     {:put    {:summary "Update a comment by the moderator"
-               :handler ok}
-      :delete {:summary "Delete a comment by yhe moderator"
+     {:put    {:summary    "Update a comment by the moderator"
+               :parameters {:path {:id int?}
+                            :body {:name              string?
+                                   :slug              string?
+                                   :text              string?
+                                   :parent-comment-id int?}}
+               :handler    ok}
+      :delete {:summary "Delete a comment by the moderator"
                :handler ok}}]]])
 
 
 (def router
   (ring/router routes
-               {:data {:muuntaja m/instance
-                       :middleware [muuntaja/format-middleware]}}))
+               {:data {:muuntaja   m/instance
+                       :coercion   coercion.spec/coercion
+                       :middleware [swagger/swagger-feature
+                                    exception/exception-middleware
+                                    muuntaja/format-negotiate-middleware
+                                    muuntaja/format-response-middleware
+                                    muuntaja/format-request-middleware
+                                    coercion/coerce-request-middleware
+                                    coercion/coerce-response-middleware]}}))
 
 (def app
   (ring/ring-handler router
@@ -47,8 +67,4 @@
   (println "Server running on port 3000"))
 
 (comment
-  (start)
-  (app {:request-method :get :uri "/pring"})
-  (r/match-by-path (r/router routes) "/api/")
-  (r/match-by-path router "/api/")
-  (r/routes router))
+  (start))
